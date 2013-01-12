89bce04eab4e138cddf549656f740fae1643d393
Wen-Tien Chang <ihower@gmail.com>
2013-01-12 14:24:51 +0800

    fix SUB_FLAGS

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 class WelcomeController < ApplicationController
 
-  before_filter :set_countries_image
   caches_page :static
 
 
@@ -9,17 +8,13 @@ class WelcomeController < ApplicationController
     push_associated_content([ 
                              [ApplicationController.helpers.asset_path("application.css"), 0], 
                              [ApplicationController.helpers.asset_path("application.js"), 1],
-                           ] + $countries_image.map{|c| [ApplicationController.helpers.asset_path("flags/#{c}"),2 ]})
+                           ] + $SUB_FLAGS.map{|c| [ApplicationController.helpers.asset_path("flags/#{c}"),2 ]})
   end
 
   protected
 
   def push_associated_content(resources)
     request.env['rack.associated_content.resources'] = resources
-  end
-
-  def set_countries_image
-    $countries_image = $FLAGS.sample(90)
   end
 
 end
